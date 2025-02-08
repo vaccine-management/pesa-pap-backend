702f7d89b4a5ada37d69e3e703dd6a26e5c49197
David <nanjiladavid2@gmail.com>
2025-02-08 03:08:08 +0300

    create pesapad backed
    Signed-off-by: David <nanjiladavid2@gmail.com>

--- index.js
+++ index.js
@@ -13,14 +13,91 @@ app.get("/", (req, res) => {
   res.send("health test 100%");
 });
 
+
+// mpesa token middleware
+
+const mpesaTokenMiddleware = async (req, res, next) => {
+    const consumerKey = process.env.MPESA_CONSUMER_KEY;
+    const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
+    
+    const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+    
+    const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64");
+    
+    try {
+        const response = await axios.get(url, {
+        headers: {
+            Authorization: `Basic ${auth}`,
+        },
+        });
+    
+        req.mpesaToken = response.data.access_token;
+    
+        next();
+    } catch (error) {
+        res.status(500).json({
+        message: "An error occurred",
+        error: error.message,
+        });
+    }       
+}
+
 // route for stk push
 
-app.post("/stk-push", (req, res) => {
-  const { phone, amount } = req.body;
+app.post("/stk-push",mpesaTokenMiddleware, async(req, res) => {
+    const { phone, amount } = req.body;
+    const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+    const token = req.mpesaToken;
 
-  res.json({
-    message: `Ksh ${amount} sent to ${phone}`,
-  });
+    const callbackUrl = "https://ngrokurl.com/stkcallback";
+
+    try {
+        
+        const timestamp = new Date()
+        .toISOString()  
+        .replace(/[^0-9]/g, "")
+        .slice(0, -3);
+    
+    
+    
+        const password = Buffer.from(
+            `${process.env.MPESA_SHORTCODE}${process.env.MPESA_PASSKEY}${timestamp}`
+          ).toString("base64");
+
+        const response = await axios.post(  
+            url,
+            {
+                BusinessShortCode: process.env.MPESA_SHORTCODE,
+                Password: password,
+                Timestamp: timestamp,
+                TransactionType: "CustomerPayBillOnline",
+                Amount: amount,
+                PartyA: phone,
+                PartyB: process.env.MPESA_SHORTCODE,
+                PhoneNumber: phone,
+                CallBackURL: "https://mycallbackurl.com",
+                AccountReference: "Test",
+                TransactionDesc: "Test",
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        console.log(response.data);
+        res.json({
+            message: "STK push initiated",
+            data: response.data,
+        });
+    } catch (error) {
+        res.status(500).json({
+        message: "An error occurred",
+        error: error.message,
+        });
+        
+    }
 });
 
 // route for sending link via whatsapp
@@ -28,19 +105,24 @@ app.post("/stk-push", (req, res) => {
 app.post("/send-link", async (req, res) => {
   const { phone, link } = req.body;
 
-  const formattedPhone = phone.replace(/^0/, "254");
-
   //   apiwap
+
+  if(!phone || !link){
+
+    return res.status(400).json({
+        message: "Phone and link are required",
+        });
+    }
 
   const baseUrl = "https://api.apiwap.com/api/v1";
 
   try {
     const response = await axios.post(
-      `${baseUrl}/send-link`,
+      `${baseUrl}/whatsapp/send-message`,
       {
-        phoneNumber: "+254712345678",
+        phoneNumber: `+${phone}`,
         message:
-          "Hello from the created instance!\nThis is a sample text message.",
+          `Hello please pay via the link below\n\n${link}.`,
         type: "text",
       },
       {
@@ -51,16 +133,29 @@ app.post("/send-link", async (req, res) => {
     );
 
     console.log(response.data);
+
+    res.json({
+        message: `Link: ${link} sent to ${phone}`,
+      });
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       message: "An error occurred",
       error: error.message,
     });
   }
-  res.json({
-    message: `Link: ${link} sent to ${formattedPhone}`,
-  });
 });
+
+app.post("/stkcallback",(req, res)=>{
+    console.log(req.body);
+    res.json({
+        message: "Callback received",
+        data: req.body,
+    });
+
+    // save the data to supabase
+
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
